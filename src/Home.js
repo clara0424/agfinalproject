7d49f97260d844daee8fc4ccfb3e58931f7ea085
clara0424 <hj2326@nyu.edu>
2024-04-24 12:28:55 +0200

    remove video reference

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Home.css';
 import background from './background.jpg';
-import perfumevideo from './perfumevideo/perfumevid.mp4'; // Corrected import statement with file extension
+// import perfumevideo from './perfumevideo/perfumevid.mp4'; // Corrected import statement with file extension
 
 function Home() {
   return (
     <div className="home-container">
-      <video controls>
+      {/* <video controls>
         <source src={perfumevideo} type="video/mp4" />
         Your browser does not support the video tag.
-      </video>
+      </video> */}
         <div className="additional-content">
             <h2 className="contact-title">Contact Me</h2>
             <p className="fragrance-info">Explore our wide range of fragrances and find the one that suits you best.</p>
